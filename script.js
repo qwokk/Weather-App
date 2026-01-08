4ef41b06227bed493e8d99d85848651ca928f88b
Assylbek <awoksorpa4ik@gmail.com>
2026-01-09 00:06:01 +0500

    feat: log latitude and longitude from geoData

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-async function getWether() {
+async function getWeather() {
     const city = document.getElementById("cityInput").value;
 
-   if(city) {
-    alert('Please enter a city name');
+   if (!city) {
+    alert("Please enter a city name");
     return;
    } 
 
    let geoURL = `https://geocoding-api.open-meteo.com/v1/search?name=${city}`;
+
+   let geoData = await fetch(geoURL).then((res) => res.json());
+
+   if (!geoData.results) {
+    alert("city not found");
+    return;
+   }
+   console.log(geoData);
 }
